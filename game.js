af14758be5fb68e1516869fe4ee303eef6636fa7
Aaronsupa <johnsona23@greenhill.org>
2023-03-06 23:59:55 -0600

    game.js updates

--- game.js
+++ game.js
@@ -5,8 +5,34 @@ function getComputerSelection(Choices){
 
 function playRound(playerSelection, computerSelection){
     console.log(`${playerSelection} ${computerSelection}`)
+    if (playerSelection == computerSelection){
+        console.log("Tie!")
+    }
+    else if (playerSelection == "rock"){
+        if (computerSelection == "paper"){
+            console.log("Player wins")
+        }
+        else {
+            console.log("Computer wins")
+        }
+    }
+    else if (playerSelection == "paper"){
+        if (computerSelection == "rock"){
+                console.log("Player wins")
+        }
+        else {
+            console.log("Computer wins")
+        }
+    }
+    else if (playerSelection == "scissors"){
+        if (computerSelection == "paper"){
+            console.log("Player wins")
+        }
+        else {
+            console.log("Computer wins")
+        }
+    }
 }
-
 
 const Choices = ["rock", "paper", "scissors"]
 let playerSelection = prompt("What's your choice? ");
